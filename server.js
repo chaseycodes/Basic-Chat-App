7e49beff7f0f9f537f98249665845cebd6adbdec
Chase Ahn <ahn.ch@Chases-MacBook-Pro.local>
2019-02-07 12:10:52 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+//Render index at root '/'
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+//Create Socket for Chat Connection
+io.on('connection', (socket)=>{
+    socket.on('chat', (msg)=>{
+        io.emit('chat', msg)
+    })
+})
+
+io.on('disconnect', function(){
+    console.log('User Disconnected')
+})
+
+http.listen(3001, ()=>{
+    console.log("listening to port:3001")
+})
