e1d2ccb588bf886b03bcf44931d68298e482bc44
Chase Ahn <ahn.ch@Chases-MacBook-Pro.local>
2019-02-07 15:42:38 -0500

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+var express = require('express')
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+
+
 
 //Render index at root '/'
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/index.html")
 })
+
+
 
 //Create Socket for Chat Connection
 io.on('connection', (socket)=>{
@@ -17,6 +22,8 @@ io.on('connection', (socket)=>{
 io.on('disconnect', function(){
     console.log('User Disconnected')
 })
+
+app.use(express.static('public'))
 
 http.listen(3001, ()=>{
     console.log("listening to port:3001")
