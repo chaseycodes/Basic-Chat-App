d79ee6e6ee5ec2efa7728480b4de5c28169b1f85
Chase Ahn <ahn.ch@Chases-MacBook-Pro.local>
2019-02-13 14:24:50 -0500

    add u/p input val

--- server.js
+++ server.js
@@ -2,49 +2,40 @@ var express = require('express')
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-const Joi = require('joi')
+const bodyParser = require('body-parser');
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const games = [
     { id: 1, name: 'chess' },
     { id: 2, name: 'tic-tac-toe' },
     { id: 3, name: 'pong' }
 ]
+
 //Render index at root '/'
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+"/login.html")
+})
+
+app.post('/', (req, res)=>{
+    var username = req.body.uname
+    var password = req.body.psw
+
+    console.log(username)
+    console.log(password)
+})
+
+app.get('/game', (req, res)=>{
+    res.sendFile(__dirname+"/game.html");
+    //Get HTML CODE FROM FLASK
+    //CREATE RENDER FUNCTION FROM HERE TO HTML
 })
 
 app.get('/games', (req, res) =>{
     res.send(games);
 })
 
-app.get('/games/:id', (req, res)=>{
-    const game = games.find(c => c.id === parseInt(req.params.id));
-    if (!game) res.status(404).send("Id not found");
-    res.send(game);
-})
-
-app.post('/games', (req, res)=>{
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message)
-        return;
-    }
-
-    const game = {
-        id: games.length + 1,
-        name: req.body.name
-    };
-    games.push(game);
-    res.send(game);
-})
 
 //Create Socket for Chat Connection
 io.on('connection', (socket)=>{
